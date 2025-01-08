8d8047bc0f33c028659bc2faa92a9409064669f4
Jiayao Yu <yjybxx2@yahoo.com>
2025-01-08 22:49:59 +0100

    Added DeerController.cs so the deer walks in a random trajectory.

---
+++ Assets/Scripts/DeerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class DeerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _directionChangeTimeInterval = 2f;
+
+    private Vector3 _direction;
+    private float _timer = 0f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetARandomDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += _direction * (_speed * Time.deltaTime);
+        
+        _timer += Time.deltaTime;
+        if (_timer >= _directionChangeTimeInterval)
+        {
+            SetARandomDirection();
+            _timer = 0;
+        }
+    }
+
+    private void SetARandomDirection()
+    {
+        _direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-0.5f, 0.5f), Random.Range(-1f, 1f)).normalized;
+        if (_direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(_direction);
+        }
+    }
+}
